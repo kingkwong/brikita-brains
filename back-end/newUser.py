cce598eae5cce10741c7f0dbd346e661c78e70e6
nikita-borisov <nikita.borisoff@gmail.com>
2016-06-05 15:38:02 -0400

    Add files via upload
    now it does a check for a vacant spot in the csv

--- back-end/newUser.py
+++ back-end/newUser.py
@@ -12,19 +12,40 @@ password = fromQS["password"].value
 owner = fromQS["owner"].value
 rpassword = fromQS["rpassword"].value
 
+def csvToDict(filename):
+    fo = open(filename, "rU")
+    csv = fo.read()
+    fo.close()
+    listOfLines = csv.split("\n")
+    output = {}
+    for line in listOfLines:
+        listOfValues = line.split(",")
+        output[listOfValues[0]] = listOfValues[1:]
+    return output
+    
 if rpassword == password:
-    inputString = "\n" + username + "," + password + "," + owner 
-    dest = open("../brikita-user-password-owner.csv", "a", 0)
-    dest.write(inputString)
-    dest.close()
+    checkVacancy = csvToDict("../brikita-user-password-owner.csv") 
+    if username in checkVacancy:
+        templateRead = open("template.txt", "rU")
+        template = templateRead.read()
+        templateRead.close()
     
-    templateRead = open("template.txt", "rU")
-    template = templateRead.read()
-    templateRead.close()
+        outputHtml = template.replace("TITLE", "error")\
+                             .replace("BODY", "<h1>This username already exists</h1><a href='http://homer.stuy.edu/~nikita.borisov/BrikitaSignUp.html'>Back</a>")
     
-    outputHtml = template.replace("TITLE", "success")\
-                         .replace("BODY", "<h1>Your sign up was successful</h1><a href='http://homer.stuy.edu/~nikita.borisov/BrikitaBrains.html'>Home</a>")
-    print outputHtml                         
+    else:
+        inputString = "\n" + username + "," + password + "," + owner 
+        dest = open("../brikita-user-password-owner.csv", "a", 0)
+        dest.write(inputString)
+        dest.close()
+    
+        templateRead = open("template.txt", "rU")
+        template = templateRead.read()
+        templateRead.close()
+    
+        outputHtml = template.replace("TITLE", "success")\
+                             .replace("BODY", "<h1>Your sign up was successful</h1><a href='http://homer.stuy.edu/~nikita.borisov/BrikitaBrains.html'>Home</a>")
+        print outputHtml                         
 else:
     templateRead = open("template.txt", "rU")
     template = templateRead.read()

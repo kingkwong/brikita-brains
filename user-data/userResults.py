ce03c660b80440a7f27e5f35a3779486e06fd861
Brian Kwong <briankwongg@gmail.com>
2016-06-09 13:40:42 -0400

    updated results page

--- user-data/userResults.py
+++ user-data/userResults.py
@@ -14,6 +14,7 @@ fromQS = cgi.FieldStorage()
     # isUser = True
 # except:
     # isUser = False
+    
 csv = open("/home/students/2018/nikita.borisov/userDatabase/bk.csv", "r")
 results = csv.read()
 
@@ -23,24 +24,50 @@ def parseResults (contents):
     for trial in listOfTrials:
         sublist = trial.split(",")[1:]
         listOfLists.append(sublist)
-    return listOfLists
-
-entry = eval(parseResults(results)[1][0].replace(";", ","))
-
-htmlTemplate = '''
-    <h1> Hi, USER. Here are your results: </h1>
-    <h2> Average Times </h2>
-    <p> Stage 1: TIME1 </p>
-    <p> Stage 2: TIME2 </p>
-    <p> Stage 3: TIME3 </p>
-'''
+    return listOfLists[1:]
 
 import timecalculator
-time1 = str(timecalculator.timeCalculator(entry[1], entry[10]))
-time2 = str(timecalculator.timeCalculator(entry[11], entry[20]))
-time3 = str(timecalculator.timeCalculator(entry[21], entry[30]))
+listOfTime1 = []
+listOfTime2 = []
+listOfTime3 = []
+data = parseResults(results)[1:]
 
-htmlFinal = htmlTemplate.replace("TIME1", time1).replace("TIME2", time2).replace("TIME3", time3)
+for entry in data:
+    time = eval(entry[0].replace(";", ","))
+    time1 = str(timecalculator.timeCalculator(time[1], time[10]))
+    time2 = str(timecalculator.timeCalculator(time[11], time[20]))
+    time3 = str(timecalculator.timeCalculator(time[21], time[30]))
+    listOfTime1.append(time1)
+    listOfTime2.append(time2)
+    listOfTime3.append(time3)
+    
+def tableGen (list1, list2, list3):
+    tableTemplate = '''
+        <table border="1">
+            <caption> </caption>
+            <tbody>
+                <tr>
+                    <th> Test 1 </th>
+                    <th> Test 2 </th>
+                    <th> Test 3 </th>
+                </tr>
+                TABLECONTENTS
+            </tbody>
+        </table>
+    '''
+    i = 0
+    finalEntry = ""
+    while i < len(list1):
+        subentry1 = "<td>" + str(list1[i]) + "</td>"
+        subentry2 = "<td>" + str(list2[i]) + "</td>"
+        subentry3 = "<td>" + str(list3[i]) + "</td>"
+        entry = "<tr>" + subentry1 + subentry2 + subentry3 + "</tr>\n"
+        finalEntry += entry
+        i += 1
+    finalTable = tableTemplate.replace("TABLECONTENTS", finalEntry)
+    return finalTable
+
+htmlFinal = tableGen(listOfTime1, listOfTime2, listOfTime3)
 print htmlFinal    
 
 # if isUser == True:
